0338854f3c1dd351f332af96c5206000275dffb9
KanavBansal1 <bansalkanav7@gmail.com>
2026-04-06 20:10:04 +0530

    Fixed chart download cloud issue

--- download.py
+++ download.py
@@ -4,11 +4,16 @@ import plotly.io as pio
 
 def download_chart(fig):
 
-    img = pio.to_image(fig, format="png")
+    try:
+        img = pio.to_image(fig, format="png")
 
-    st.download_button(
-        label="Download Chart",
-        data=img,
-        file_name="dashboard.png",
-        mime="image/png"
-    )
+        st.download_button(
+            label="Download Chart",
+            data=img,
+            file_name="dashboard.png",
+            mime="image/png"
+        )
+
+    except Exception:
+
+        st.info("Chart download not supported in cloud. Download data instead.")
--- llm.py
+++ llm.py
@@ -1,10 +1,10 @@
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
 import os
+import streamlit as st
+from langchain_groq import ChatGroq
 
-load_dotenv()
+api_key = os.getenv("GROQ_API_KEY") or st.secrets["GROQ_API_KEY"]
 
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
-    temperature=0
+    groq_api_key=api_key,
+    model_name="llama3-8b-8192"
 )
